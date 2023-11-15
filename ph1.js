81606f32f917914d7c832ea856bae895ec7ef248
aueaoangkung <sroiaudom@gmail.com>
2023-11-15 14:12:07 +0700

    Test

--- ph1.js
+++ ph1.js
@@ -1,9 +1,9 @@
 let knex = require('knex')({
     client: 'mssql',
     connection: {
-        server : '10.28.99.42',
-        user : 'aueaoangkun_s',
-        password : '0822914530aA',
+        server : 'AUEAOANGKUN',
+        user : 'sroiaudom',
+        password : '0822914530',
         database : 'NPS_SOLAR',
     }
 });
@@ -78,7 +78,7 @@ async function writeDB() {
             
             console.log(sql)
 
-            await knex('Test').insert({
+            await knex('Ph_Dv').insert({
                 PowerHouse : 'PowerHouse1',
                 DateTime : date,
                 CodeTime : minutes,
